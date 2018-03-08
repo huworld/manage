5368a439c87e958410e63d1114c5ec57565a80d5
dahu <webworld_hu@163.com>
2018-03-08 14:22:07 +0800

    wuhaha

--- pages/components/calender/calender.js
+++ pages/components/calender/calender.js
@@ -1,4 +1,19 @@
 // pages/components/calender.js
 Component({
-   
+   data:{
+      weekArr:["日","一","二","三","四","五","六"]
+   },
+   created:function(){
+      console.log("A")
+      let T = new Date()
+      let year = T.getFullYear()
+      //判断闰年
+      let yeartype = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
+      console.log(yeartype)
+      //
+   },
+   attached:function(){
+      console.log("a")
+      console.log(yeartype)
+   }
 })
